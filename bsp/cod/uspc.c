54da5c84fdaff6274b4fbb6a3cde0398680d2dc2
mazent <massimo.zennaro@texa.com>
2018-06-14 15:37:53 +0200

    debug

--- bsp/cod/uspc.c
+++ bsp/cod/uspc.c
@@ -134,7 +134,8 @@ bool USPC_beg(uint32_t baud, USPC_RX_CB cb)
 			.data_bits = UART_DATA_8_BITS,
 			.parity    = UART_PARITY_DISABLE,
 			.stop_bits = UART_STOP_BITS_1,
-			.flow_ctrl = UART_HW_FLOWCTRL_DISABLE
+			.flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+			.rx_flow_ctrl_thresh = 120
 	};
 
 	do {
