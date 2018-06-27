33513123932b04206f1355dd5bc64183c52d6a86
mazent <massimo.zennaro@texa.com>
2018-06-27 15:35:48 +0200

    xxx

--- bsp/cod/mobd.c
+++ bsp/cod/mobd.c
@@ -1,5 +1,9 @@
 #include "mobd.h"
 
+#include "driver/gpio.h"
+
+#define SC635		0
+
 #define MOBD_SEL		GPIO_SEL_33
 #define MOBD    		GPIO_NUM_33
 
@@ -7,7 +11,11 @@
 #define ETH    			GPIO_NUM_5
 
 static const gpio_config_t cfg = {
+#if SC635 == 1
 	.pin_bit_mask = MOBD_SEL | ETH_SEL,
+#else
+	.pin_bit_mask = ETH_SEL,
+#endif
 	.mode = GPIO_MODE_OUTPUT,
 	.intr_type = GPIO_INTR_DISABLE,
 	.pull_up_en = GPIO_PULLUP_DISABLE,
@@ -28,7 +36,9 @@ void MOBD_end(void)
 
 void MOBD_mobd_eth(bool eth)
 {
+#if SC635 == 1
 	gpio_set_level(MOBD, eth ? 1 : 0) ;
+#endif
 }
 
 void MOBD_eth_esp32(bool esp32)
