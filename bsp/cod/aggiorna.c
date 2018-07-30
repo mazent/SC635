98d1e3a25167a2a833da7a4f197fe0c552d55690
mazent <massimo.zennaro@texa.com>
2018-07-30 14:44:46 +0200

    cifrato

--- bsp/cod/aggiorna.c
+++ bsp/cod/aggiorna.c
@@ -2,10 +2,44 @@
 #include "aggiorna.h"
 #include "esp_ota_ops.h"
 
-//static const char *TAG = "agg";
+static const char *TAG = "agg";
 
 static uint8_t * nuovo = NULL ;
 static uint32_t dimNuovo = 0 ;
+
+static const uint8_t key[32] = {
+	0x57, 0xF4, 0x0F, 0xF2, 0x91, 0xBF, 0xDC, 0x8E, 0x69, 0x12, 0x1C, 0xC4, 0xE3, 0x99, 0x05, 0x05,
+	0xEA, 0xEA, 0x82, 0x3A, 0x15, 0x1A, 0x39, 0x6B, 0xA9, 0xFE, 0xE4, 0x68, 0x18, 0x75, 0xF4, 0x08
+};
+
+static bool decifra(void)
+{
+	bool esito = false ;
+	mbedtls_aes_context aes = { 0 } ;
+	const uint8_t * iv = nuovo ;
+	const uint8_t * ct = nuovo + 16 ;
+	const uint32_t DIM = dimNuovo - 16 ;
+	uint8_t * pt = NULL ;
+
+	do {
+		pt = os_malloc(dimNuovo) ;
+		if (NULL == pt) {
+			DBG_ERR ;
+			break ;
+		}
+
+		mbedtls_aes_init(&aes) ;
+
+		(void) mbedtls_aes_setkey_dec(&aes, key, 256) ;
+
+		esito = 0 == mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_DECRYPT, DIM, iv, ct, pt) ;
+
+	} while (false) ;
+
+	mbedtls_aes_free(&aes) ;
+
+	return esito ;
+}
 
 void AGG_beg(uint32_t dim)
 {
@@ -49,11 +83,17 @@ bool AGG_end(void)
 		if (NULL == nuovo)
 			break ;
 
+		if ( !decifra() ) {
+			DBG_ERR ;
+			break ;
+		}
+
 		const esp_partition_t * part = esp_ota_get_next_update_partition(NULL) ;
 		if (NULL == part) {
 			DBG_ERR ;
 			break ;
 		}
+		ESP_LOGI(TAG, "partizione da %d byte", part->size) ;
 
 		if (part->size < dimNuovo) {
 			DBG_ERR ;
